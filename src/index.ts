80993425e4659ea155cf820f4929fb8e124d865c
Ihor Murzich <murzichigor@outlook.com>
2018-05-10 13:20:39 +0300

    init commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import './style.css';
+import createBattlefieldGrid from "./ts/gridDOM";
+
+createBattlefieldGrid();
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'cheap-eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.tsx?$/,
+                use: ['ts-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    devServer: {
+        contentBase: './dist',
+        index: './dist/index.html',
+        port: 3000,
+        disableHostCheck: true,
+        host: '0.0.0.0'
+    }
+};
