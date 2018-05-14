dafd932b8a78a4da47d75897a53dc1bf42f18270
Ihor Murzich <murzichigor@outlook.com>
2018-05-14 16:07:03 +0300

    added webpack production config

---
+++ webpack.prod.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.tsx?$/,
+                use: ['ts-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+};
